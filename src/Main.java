f2fc6ff57cb59fbaa6d2865eeae63edc8dd32827
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2024-07-23 12:03:09 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Doğal Dil Hesaplayıcı'ya Hoş Geldiniz!");
+        System.out.println("Hoş Geldiniz!");
         // Diğer adımlar burada tamamlanacak
     }
 }
