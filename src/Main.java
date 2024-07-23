18936b80887e3f76e13b907ab172b70b54a5a86e
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2024-07-23 12:44:48 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,54 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
+    // Sayı Map
+    private static final Map<String, Integer> numberMap = new HashMap<>();
+    // İşlem Map
+    private static final Map<String, String> operatorMap = new HashMap<>();
+
+    static {
+        // Sayı Map
+        numberMap.put("zero", 0);
+        numberMap.put("one", 1);
+        numberMap.put("two", 2);
+        numberMap.put("three", 3);
+        numberMap.put("four", 4);
+        numberMap.put("five", 5);
+        numberMap.put("six", 6);
+        numberMap.put("seven", 7);
+        numberMap.put("eight", 8);
+        numberMap.put("nine", 9);
+        numberMap.put("ten", 10);
+
+        // İşlem Map
+        operatorMap.put("add", "+");
+        operatorMap.put("plus", "+");
+        operatorMap.put("subtract", "-");
+        operatorMap.put("minus", "-");
+        operatorMap.put("less", "-");
+        operatorMap.put("multiplied-by", "*");
+        operatorMap.put("times", "*");
+        operatorMap.put("divided-by", "/");
+        operatorMap.put("over", "/");
+    }
     public static void main(String[] args) {
-        // Kullanıcıdan girdi almak için Scanner sınıfını kullanacağız.
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-        System.out.println("Lütfen bir hesaplama girin: ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hesaplama girin: ");
         String input = scanner.nextLine();
 
-        System.out.println("Girdi: " + input);
+        // Girdiyi kelimelere bölme
+        String[] words = input.split(" ");
+
+        for (String word : words) {
+            word = word.toLowerCase();
+            if (numberMap.containsKey(word)) {
+                System.out.println("Sayı: " + numberMap.get(word));
+            } else if (operatorMap.containsKey(word)) {
+                System.out.println("İşlem: " + operatorMap.get(word));
+            }
+        }
 
         scanner.close();
     }
